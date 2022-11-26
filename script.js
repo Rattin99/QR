cad99b3dae5e6bb8d82f004651861d3ae8cd50ab
Rattin99 <rattin99m18@gmail.com>
2022-11-27 00:46:25 +0600

    last

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
 
-document.querySelector('video').play();
-
 setTimeout(() => {
     window.location.replace("https://www.facebook.com/%E0%A6%B0%E0%A6%A3%E0%A6%A8-%E0%A7%A7%E0%A7%AE-106385205641734/")
 },7000)
